2cb1a6817820dd1917794aaf328764765a82bee4
LTI <LTI@INFEDL31041.lntinfotech.com>
2021-09-20 15:51:54 -0500

    converts Roman Numeral to integers

--- RomanNumerals/Program.cs
+++ RomanNumerals/Program.cs
@@ -6,9 +6,67 @@ namespace RomanNumerals
     {
         static void Main(string[] args)
         {
-
-            
-            Console.WriteLine("Hello World!");
+            string romanNumber = "MDCCCLXXXVIII";
+            Console.WriteLine(Program.ConversionResults(romanNumber));
         }
+        static int ConvertLetter(char letter)
+        {
+            if (letter == 'I')
+            {
+                return 1;
+            }
+            else if (letter == 'V')
+            {
+                return 5;
+            }
+            else if (letter == 'X')
+            {
+                return 10;
+            }
+            else if (letter == 'L')
+            {
+                return 50;
+            }
+            else if (letter == 'C')
+            {
+                return 100;
+            }
+            else if (letter == 'D')
+            {
+                return 500;
+            }
+            else if (letter == 'M')
+            {
+                return 1000;
+            }
+            return -1;
+
+        }
+        static int ConversionResults(string romanNum)
+        {
+            romanNum = romanNum.ToUpper();
+            int result = 0;
+            foreach (var letter in romanNum)
+            {
+                result += ConvertLetter(letter);
+            }
+
+            if (romanNum.Contains("IV") || romanNum.Contains("IX"))
+            {
+                result = result - 2;
+            }
+            if (romanNum.Contains("XL") || romanNum.Contains("XC"))
+            {
+                result = result - 20;
+            }
+            if (romanNum.Contains("CD") || romanNum.Contains("CM"))
+            {
+                result = result - 200;
+            }
+            return result;
+
+        }
+
+
     }
 }
